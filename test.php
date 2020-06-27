7ea8c0381342a51d6efc78c85818e67bc2fa8efd
CarlosMG <36088774+cmayoralg@users.noreply.github.com>
2020-06-27 20:25:32 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+    /*echo "FUNCIONO";
+    $con = mysqli_connect("localhost","root","root","test");
+
+    if (mysqli_connect_errno()){
+        echo ("1:Connection failed");
+        exit();
+    }
+
+    $username = $_POST["name"];
+    $password = $_POST["password"];
+
+    $namecheckquery = "SELECT name from players where name ="+$username+";";
+
+    $namecheck = mysqli_query($con,$namecheckquery) or die ("2:Name check query failed");
+
+    if (mysqli_num_rows($namecheck)>0){
+        echo "3:name exists";
+        exit();
+    }*/
+    //$username = $_POST["name"];
+    echo "Tu nombre es Amparito ";
+
+?>
